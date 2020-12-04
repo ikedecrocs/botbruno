43d52feb22106afd008e2cb84c03542515a30af2
mateus.coelho <mateus.coelho>
2020-12-04 13:23:25 -0300

    Enviando mensagem para todos os canais

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//quando ficar on, é chamado
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//toda mensagem, é chamado
+client.on('message', (msg) => streamWarning(msg));
+
+// function streamWarning(msg) {
+//     if (msg.content === '!live') {
+//         var messageToSend = '@everyone :point_right: :pineapple: https://www.twitch.tv/reiabacaxi :pineapple: :point_left:';
+//         //client.guilds.cache.forEach(guild => guild.defaultChannel.send(messageToSend));
+//         client.guilds.cache.forEach(guild => console.log(guild.name))
+//     }
+// }
+
+function streamWarning(msg) {
+    if (msg.content === "!live") {
+        try {
+            client.guilds.cache.forEach((guild) => {
+                var messageToSend = ':point_right: :pineapple: https://www.twitch.tv/reiabacaxi :pineapple: :point_left:'; //marcar pessoal depois
+                //guild.channels.cache.forEach((channel) => channel.send(messageToSend));
+                guild.channels.cache.forEach(channel => {
+                    if (channel.isText()) {
+                        channel.send(messageToSend);
+                        
+                    }
+                    // console.log(channel.isText());
+                });
+                // let channel = guild.channels.cache.find(channel => channel.name === 'rei-abacaxi');
+                // if (channel != undefined) {
+                //     channel.send(messageToSend);
+                // }
+            });
+        } catch (err) {
+            console.log("error");
+        }
+    }
+}
+
+client.login('Nzg0MTk4MDExMjAyNDM3MTMw.X8lzTw.q-B1B2IOtMRVQ0b0FQLf3MlmbmU');
